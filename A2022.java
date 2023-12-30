1fe14742def287bb81bc412b997a5f8debf57093
JEON-MIN-SEO <jmsparrowk@naver.com>
2023-12-30 20:39:30 +0900

    12/30-5day

---
+++ A2022.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class A2022 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str =scanner.nextLine();
+
+        System.out.println(str);
+    }
+}
---
+++ A2023.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class A2023 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double input = scanner.nextDouble();
+
+        int num1 = (int)input;
+        //double num2 = input - (int)input;
+        
+        String num2 = String.valueOf(input).substring(String.valueOf(input).indexOf(".")+1);
+        // String.valueOf(intput)の意味はdoubleのinputをstringに変える
+        //　substring(...)はメソッドの最初のインデくそインデックスから最後のインデックスまで返還
+        // indexOf(".")+1特定の文字を探すときに使用 +1は　.次から返還するため
+
+        System.out.println(num1 + "\n" + num2);
+        
+
+        //String st = "abcdefg";
+        //System.out.println(st.substring(2));
+        //result=cdefg
+    }
+    
+}
