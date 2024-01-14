4a41aa6d1dab7a1207d6932793c972b47705fc69
JEON-MIN-SEO <jmsparrowk@naver.com>
2024-01-14 23:58:19 +0900

    1/14-16day

---
+++ A1056.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class A1056{
+    public static void main (String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        
+        if(a!=b){
+            System.out.print("1");
+        }else{
+            System.out.print("0");
+        }
+    }
+}
